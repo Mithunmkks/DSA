5fb95d028e1bc8dad83850300ced0f2d8620aa58
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-05-29 23:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (72.01%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -14,14 +14,32 @@ public:
     vector<int> postorderTraversal(TreeNode* root) {
         if(root==NULL)return {};
         stack<TreeNode*> s1,s2;
-        s1.push(root);
-        while(!s1.empty())
+        auto cur = root;
+        while(!s1.empty() || cur)
         {
-            auto node = s1.top();
-            s1.pop();
-            s2.push(node);
-            if(node->left)s1.push(node->left);
-            if(node->right)s1.push(node->right);
+            if(cur)
+            {
+                s1.push(cur);
+                cur=cur->left;
+            }
+            else 
+            {
+
+                if(s1.top()->right)
+                {
+                        if(!s2.empty() && (s1.top()->right==s2.top()))
+                        {
+                            s2.push(s1.top());
+                            s1.pop();
+                        }
+                        else cur=s1.top()->right;
+                }
+                else 
+                {
+                    s2.push(s1.top());
+                    s1.pop();
+                }
+            }
         }
         vector<int> ans;
         while(!s2.empty())
@@ -29,6 +47,7 @@ public:
             ans.push_back(s2.top()->val);
             s2.pop();
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
