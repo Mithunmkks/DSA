f717ce4b6009098c02030705b9d7b892a6166d33
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-06-10 21:55:09 +0530

    Time: 562 ms (55.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select employee_id , department_id
+from Employee
+where primary_flag='Y'
+union all 
+select employee_id , department_id 
+from Employee 
+group by 1
+having count(*)=1;
+
