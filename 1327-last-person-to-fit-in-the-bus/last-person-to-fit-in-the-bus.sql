d9e87e8247734c61e6b7e55f5044520c156e3c22
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-06-11 21:22:45 +0530

    Time: 752 ms (89.08%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,22 @@
-# Write your MySQL query statement below
-with cte as (
-select * , sum(weight) over(order by turn) as rs 
-from Queue
-),
-cte2 as (
-select * , lead(rs) over() as nw 
-from cte )
-select person_name 
-from cte2
-where rs<=1000 and (nw > 1000 OR nw IS NULL);
+-- # Write your MySQL query statement below
+-- with cte as (
+-- select * , sum(weight) over(order by turn) as rs 
+-- from Queue
+-- ),
+-- cte2 as (
+-- select * , lead(rs) over() as nw 
+-- from cte )
+-- select person_name 
+-- from cte2
+-- where rs<=1000 and (nw > 1000 OR nw IS NULL);
+
+
+with cte as (select *,
+sum(weight) over(order by turn) as rol_sum
+from queue)
+
+
+select person_name from cte
+where rol_sum <=1000
+order by rol_sum desc
+limit 1
