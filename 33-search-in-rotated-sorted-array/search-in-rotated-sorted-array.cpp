5d60b0fecd78c1df7e20e1b33e199afdcc113824
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-06-23 18:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int mid,l=0,h=n-1;
-        while(l<h)
-        {
-            mid=(l+h)/2;
-            if(nums[mid]>nums[h])l=mid+1;
-            else h=mid;
-        }
-        int pivot = l;
-        l=0,h=pivot-1;
+        int l=0,h=nums.size()-1;
         while(l<=h)
         {
-            mid=(l+h)/2;
+            int mid = l+(h-l)/2;
             if(nums[mid]==target)return mid;
-            if(nums[mid]<target)l=mid+1;
-            else h=mid-1;
-        }
-        l=pivot,h=n-1;
-        while(l<=h)
-        {
-            mid=(l+h)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[mid]<target)l=mid+1;
-            else h=mid-1;
+            if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target && nums[mid]>=target)h=mid;
+                else l=mid+1;
+            }
+            else 
+            {
+                if(nums[mid]<=target && target<=nums[h])
+                {
+                    l=mid+1;
+                }
+                else h=mid;
+            }
         }
         return -1;
-        
+
+    
     }
 };
-
-
-// 56,58,59,100,123,127,198,199,1,2,3,4,5,6,7,8
-// [4,5,6,7,0,1,2]
-//  0 1 2 3 4 5 6
-//.         ..   .
