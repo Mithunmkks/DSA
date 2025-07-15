de99face3d90777b8676a9b307b843042addda5b
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-07-15 15:55:46 +0530

    Time: 68 ms (77.30%) | Memory: 173.1 MB (72.51%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,94 +1,76 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
 class LRUCache {
 public:
-    class ListNode{
-        public:
-        int key;
-        int val;
-        ListNode* next;
-        ListNode* prev;
-        ListNode(int key , int val)
-        {
-            this->key = key;
-            this->val = val;
-            this->next = NULL;
-            this->prev = NULL;
+
+    class Node {
+    public:
+        int key, val;
+        Node* prev;
+        Node* next;
+
+        Node(int k, int v) {
+            key = k;
+            val = v;
+            prev = nullptr;
+            next = nullptr;
         }
     };
-    void insertAtHead(ListNode*&head,ListNode*&node)
-    {
-        node->next=head->next;
-        node->prev=head;
-        head->next->prev=node;
-        head->next=node;
-    }
-    void clean(ListNode*&node)
-    {
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
-        node->next=NULL;
-        node->prev=NULL;
-    }
-    void deletion(ListNode*&node)
-    {
-        node->prev->next=tail;
-        tail->prev=node->prev;
-        node->next=NULL;
-        node->prev=NULL;
-    }
-    
-    unordered_map<int,ListNode*>mp;
-    int capacity;
-    ListNode* head = NULL;
-    ListNode* tail = NULL;
 
-    LRUCache(int capacity) {
-        head = new ListNode(-1,-1);
-        tail = new ListNode(-1,-1);
+    unordered_map<int, Node*> mp;
+    int capacity;
+    Node* head;
+    Node* tail;
+
+    LRUCache(int cap) {
+        capacity = cap;
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
         head->next = tail;
         tail->prev = head;
-        this->capacity = capacity;
     }
-    
-    int get(int key) {
-        if(mp.find(key)!=mp.end())
-        {
-            int r = mp[key]->val;
-            clean(mp[key]);
-            insertAtHead(head,mp[key]);
-            return r;
-        }
-        else return -1;
-    }
-    
-    void put(int key, int value) {
-        if(mp.find(key)!=mp.end())
-        {
-            mp[key]->val=value;
-            clean(mp[key]);
-            insertAtHead(head,mp[key]);
-            return ;
-        }
-        if(capacity<=0)
-        {
-            ListNode* node = tail->prev;
-            deletion(node);
-            mp.erase(node->key);
-        }
-        ListNode* node = new ListNode(key,value);
-        ListNode* nextnode = head->next;
-        node->next = nextnode;
-        head->next = node;
-        node->prev=head;
-        nextnode->prev = node; 
-        mp[key]=node;
-        capacity--;
 
+    void remove(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void insert_at_front(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    int get(int key) {
+        if (mp.find(key) != mp.end()) {
+            Node* node = mp[key];
+            remove(node);            // Remove from current position
+            insert_at_front(node);   // Insert at front
+            return node->val;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            Node* node = mp[key];
+            node->val = value;
+            remove(node);
+            insert_at_front(node);
+        } else {
+            if (mp.size() == capacity) {
+                Node* lru = tail->prev;
+                remove(lru);
+                mp.erase(lru->key);
+                delete lru;
+            }
+            Node* node = new Node(key, value);
+            insert_at_front(node);
+            mp[key] = node;
+        }
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
