b4ab4ba4ac0252769ecb12841b0bc8bfefa42e1c
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-07-16 17:08:15 +0530

    Time: 8 ms (76.53%) | Memory: 11.8 MB (58.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool solve(int i,int j,string&s)
-    {
-        if(i>=j)return true;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s[i]!=s[j])return dp[i][j]=false;
-        return dp[i][j]= solve(i+1,j-1,s);
-    }
     string longestPalindrome(string s) {
+        int len =0;
+        string ans="";
         int n = s.size();
-        memset(dp,-1,sizeof(dp));
-        int start = 0;
-        int len  = 1;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<s.size();i++)
         {
-            for(int j=0;j<n;j++)
+            int sz =1;
+            int j=i-1,k=i+1;
+            while(j>=0 && k<n && s[j]==s[k])
             {
-                if(solve(i,j,s))
-                {
-                    if(j-i+1>len)
-                    {
-                        len=j-i+1;
-                        start=i;
-                    }
-                }
+                sz+=2;
+                j--;
+                k++;
+            }
+            if(sz>len)
+            {
+                len = sz;
+                ans=s.substr(j+1,sz);
             }
         }
-        return s.substr(start,len);
+        for(int i=0;i<s.size();i++)
+        {
+            int sz =0;
+            int j=i,k=i+1;
+            while(j>=0 && k<n && s[j]==s[k])
+            {
+                sz+=2;
+                j--;
+                k++;
+            }
+            if(sz>len)
+            {
+                len = sz;
+                ans=s.substr(j+1,sz);
+            }
+        }
+
+        return ans;
+
+
+
     }
 };
