d4c32df2b08a9e6f4ec9b394ceee4892fd03be87
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-07-22 16:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (37.35%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -6,29 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int, int> mp;
-        for (int i = 0; i < inorder.size(); i++)
-        mp[inorder[i]] = i;
-auto root = buildTree(0, preorder.size() - 1, preorder, 0,inorder.size() - 1, inorder, mp);
+    map<int,int> mp;
+    TreeNode* f(int prestart , int preEnd , vector<int>&preorder , int instart ,int inEnd , vector<int> &inorder)
+    {
+        if(prestart>preEnd || instart>inEnd)return NULL;
+        int rootInd = mp[preorder[prestart]];
+        int len = rootInd-instart;
+        TreeNode* root = new TreeNode(inorder[rootInd]);
+        root->left = f(prestart+1,prestart+len,preorder,instart,rootInd-1,inorder);
+        root->right = f(prestart+len+1,preEnd,preorder,rootInd+1,inEnd,inorder);
         return root;
     }
-    TreeNode* buildTree(int ps, int pe, vector<int>& preorder,int is, int ie,
-    vector<int>& inorder, map<int, int>& mp)
-    {   
-        if(ps>pe || is>ie)return NULL;
-        TreeNode* node = new TreeNode(preorder[ps]);
-        int ir = mp[preorder[ps]];
-        int numsleft = ir-is;
-
-        node->left = buildTree(ps+1,ps+numsleft,preorder,is,ir-1,inorder,mp);
-        node->right = buildTree(ps+1+numsleft,pe,preorder,ir+1,ie,inorder,mp);
-        return node;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        for(int i=0;i<n;i++)mp[inorder[i]]=i;
+        TreeNode *root = f(0,n-1,preorder,0,n-1,inorder);
+        return root;
     }
 };
