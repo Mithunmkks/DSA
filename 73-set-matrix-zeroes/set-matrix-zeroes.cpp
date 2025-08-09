adde155e259f0d2f19b0dc4ef23523ec8451c1dc
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-10 00:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        vector<int> row(n,0),cols(m,0);
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    row[i]=1;
-                    cols[j]=1;
+    void setZeroes(vector<vector<int>>& mat) {
+        bool isRowZero = false;
+        bool isColZero = false;
+        int n = mat.size();
+        int m = mat[0].size();
+        for (int i = 0; i < n; i++) {
+            if (mat[i][0] == 0)
+                isRowZero = true;
+        }
+        for (int i = 0; i < m; i++) {
+            if (mat[0][i] == 0)
+                isColZero = true;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
                 }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(row[i]==1 || cols[j]==1)
-                {
-                    matrix[i][j]=0;
+        for (int i = 1; i < n; i++) {
+            if (mat[i][0] == 0) {
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = 0;
                 }
             }
         }
-        return;
+        for (int j = 1; j < m; j++) {
+            if (mat[0][j] == 0) {
+                for (int i = 0; i < n; i++) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        if (isRowZero) {
+            for (int i = 0; i < n; i++) {
+                mat[i][0] = 0;
+            }
+        }
+        if (isColZero) {
+            for (int i = 0; i < m; i++) {
+                mat[0][i] = 0;
+            }
+        }
     }
 };
