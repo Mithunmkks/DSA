6b47f9ff4ed9380b5a2731b6f74ddc5890802f06
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-11 02:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (36.40%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-    int minJumps(int ind,vector<int>&nums){
-        if(ind>=nums.size() || nums[ind]==0){
-            return 1e6;
-        }
-        if(ind==nums.size()-1){
-            return 0;
-        }
-        int ans = INT_MAX;
-        for(int i=1;i<=nums[ind];i++){
-            int jump = 1+minJumps(ind+i,nums);
-            ans=min(jump,ans);
-        }
-        return ans;
-    }
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return 0;
-        vector<int> dp(n,0);
-        for(int i=n-2;i>=0;i--){
-            int jump = INT_MAX;
-            if(nums[i]==0){
-                dp[i]=1e6;
-                continue;
+        int jumps=0,l=0,r=0,n=nums.size();
+        while(r<n-1){
+            int farthest = 0;
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest,i+nums[i]);
             }
-            for(int j=1;j<=nums[i];j++){
-                if(i+j<n)jump=min(jump,dp[i+j]);
-            }
-            dp[i]=1+jump;
+            l=r+1;
+            r=farthest;
+            jumps++;
         }
-        return dp[0];
+        return jumps;
     }
 };
