9db49f48386037a0e0e72e9f1d35f5b3243ca181
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-18 22:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (78.29%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int maxiProfit(int ind,int pre,vector<int> &prices,vector<vector<int>> &dp2){
+    int dp[5001][2];
+    int f(int ind,int buy,vector<int>&prices){
         if(ind>=prices.size())return 0;
-        if(dp2[ind][pre+1]!=-1)return dp2[ind][pre+1];
-        int buy = 0;
-        int sell =0;
-        int notBuy = maxiProfit(ind+1,pre,prices,dp2);
-        if(pre==-1){
-            buy =  maxiProfit(ind+1,ind,prices,dp2);
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+        int profit = 0;
+        if(buy){
+            profit=max(profit,-prices[ind]+f(ind+1,0,prices));
+            profit=max(profit,f(ind+1,1,prices));
         }
-        else{
-            sell = prices[ind]-prices[pre] + maxiProfit(ind+2,-1,prices,dp2);
+        else {
+            profit=max(profit,prices[ind]+f(ind+2,1,prices));
+            profit=max(profit,f(ind+1,0,prices));
         }
-        return dp2[ind][pre+1]=max({notBuy,buy,sell});
+        return dp[ind][buy]=profit;
     }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp2(n,vector<int>(n+1,-1));
-        return maxiProfit(0,-1,prices,dp2);
+memset(dp, -1, sizeof(dp));
+return f(0,1,prices);
 
-        // vector<vector<int>> dp(n+5,vector<int>(n+5,0));
-        // for(int i=n-1;i>=0;i--){
-        //     for(int pre=-1;pre<i;pre++){
-        //         int buy = 0;
-        //         int sell = 0;
-        //         if(pre==-1){
-        //             buy = dp[i+1][i+1];
-        //         }
-        //         else{
-        //             sell = prices[i]-prices[pre]+dp[i+2][pre+1];
-        //         }
-        //         dp[i][pre]=max(buy,sell);
-        //     }
-        // }
-        // return dp[0][0];
     }
 };
