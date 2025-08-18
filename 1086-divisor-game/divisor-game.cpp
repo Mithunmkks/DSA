dc68b9ebd7d885020c9341245669903398d51612
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-18 16:38:46 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (7.81%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -12,7 +12,7 @@ public:
     bool divisorGame(int n) {
         vector<int> dp(n+1,0);
         for(int i=2;i<=n;i++){
-            for(int j=1;j<i;j++){
+            for(int j=1;j<=i/2;j++){
                 if(i%j==0){
                     if(dp[i-j]==0){
                         dp[i]=1;
