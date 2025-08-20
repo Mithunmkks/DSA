325bff6de549ecfa57ae04c6e25140174fc5577b
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-20 23:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (54.52%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
-    // n can be zero 
     int countSegments(string s) {
-        int n = s.size();
-        if(n==0)return 0;
-        while(s.size()>0 && s.back()==' ')s.pop_back();
-        int i=0;
-        while(i<s.size() && s[i]==' ')i++;
-        s=s.substr(i);
-        n = s.size();
-        int count = 0;
-        for(int i=0;i<n;i++){
-            if(i>0 && s[i]==' ' && s[i-1]!=' ')count++;
+        int count =0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' ' && (i==0 || s[i-1]==' '))count++;
         }
-        if(count==0 && s.size()>0) return 1;
-        else if(count==0 && s.size()==0)return 0;
-        else return count+1;
-
+        return count;
         
     }
 };
