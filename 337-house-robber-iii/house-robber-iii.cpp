98ab1656914de6684c1d7851d963cb6dc749d17a
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-25 16:41:20 +0530

    Time: 15 ms (16.15%) | Memory: 31.9 MB (8.44%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -11,17 +11,18 @@
  */
 class Solution {
 public:
-    int maxMoney(TreeNode* root,int prevRob,map<pair<TreeNode*,int>,int>&mp){
-
-        if(!root)return 0;
-        if(mp.find({root,prevRob})!=mp.end())return mp[{root,prevRob}];
-        int take = 0;
-        if(!prevRob)take = root->val + maxMoney(root->left,1,mp) + maxMoney(root->right,1,mp);
-        int notTake = maxMoney(root->left,0,mp) + maxMoney(root->right,0,mp);
-        return mp[{root,prevRob}] = max(take,notTake);
+    // {withRoot,withoutRoot}
+    vector<int> maxRob(TreeNode* root){
+        if(!root)return {0,0};
+        // 0 -> with root , 1->without root
+        vector<int> lr = maxRob(root->left);
+        vector<int> rr = maxRob(root->right);
+        int withRoot = root->val + lr[1] + rr[1];
+        int withOutRoot = max({lr[0]+rr[0],lr[1]+rr[1],lr[0]+rr[1],lr[1]+rr[0]});
+        return {withRoot,withOutRoot};
     }
     int rob(TreeNode* root) {
-        map<pair<TreeNode*,int> , int > mp;
-        return maxMoney(root,0,mp);
+        vector<int> ans = maxRob(root);
+        return max(ans[0],ans[1]);
     }
 };
