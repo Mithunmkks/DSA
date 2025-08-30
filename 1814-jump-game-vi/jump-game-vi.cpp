1ddfb77b5e14d1273d300d20b8b061081a354a78
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-08-31 01:42:21 +0530

    Time: 16 ms (67.24%) | Memory: 81.8 MB (83.67%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.cpp
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        int n=nums.size();
-        deque<int> q;
-        vector<int> dp(n);
-        dp[n-1]=nums[n-1];
-        q.push_back(n-1);
+        int n = nums.size();
+        deque<pair<int,int>> dq;
+        // score,index
+        dq.push_back({nums[n-1],n-1});
+        int score = nums[0];
         for(int i=n-2;i>=0;i--){
-            if(q.front()-i>k)q.pop_front();
-            dp[i]=nums[i]+dp[q.front()];
-            while(!q.empty()&&dp[i]>dp[q.back()])q.pop_back();
-            q.push_back(i);
+            while(!dq.empty() && dq.front().second>i+k)dq.pop_front();
+            score = nums[i]+dq.front().first;
+            while(!dq.empty() && dq.back().first<=score)dq.pop_back();
+            dq.push_back({score,i});
         }
-        return dp[0];
+        return score;
+        
     }
 };
