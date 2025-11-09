a18f25283736db7396de7b319ae01b1e719cb200
MITHUN GUPTA <91526970+Mithunmkks@users.noreply.github.com>
2025-11-09 20:07:36 +0530

    Time: 5 ms (80.42%) | Memory: 30.9 MB (62.22%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int rows = matrix.size(), cols = rows > 0 ? matrix[0].size() : 0;
-        vector<vector<int>> dp(rows + 1, vector<int>(cols + 1, 0));
-        int maxsqlen = 0;
-        // for convenience, we add an extra all zero column and row
-        // outside of the actual dp table, to simpify the transition
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i - 1][j - 1] == '1') {
-                    dp[i][j] =
-                        min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) +
-                        1;
-                    maxsqlen = max(maxsqlen, dp[i][j]);
+        int n = matrix.size();
+        int m = matrix[0].size();
+         int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1'){
+                    dp[i][j] = 1;
+                    ans=1; 
                 }
             }
         }
-        return maxsqlen * maxsqlen;
+       
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+               if(dp[i][j]==1){
+                dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+               
+               } 
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        return ans*ans;
     }
 };
